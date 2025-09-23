91c079d1890b6f4b8a769a24721421045110cf33
Taher Chabra <redsign12345@gmail.com>
2025-09-24 00:29:55 +0530

    feat: initialize Express application with CORS, cookie parsing, and routing setup

---
+++ backend/src/app.ts
@@ -0,0 +1,27 @@
+import express, { Express } from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+const app: Express = express();
+
+app.use(cors({
+   origin: '*',
+   credentials: true
+}))
+app.use(express.json({ limit: '1mb' }));
+app.use(express.urlencoded({ extended: true, limit: '1mb' }));
+
+app.use(cookieParser());
+
+//routes import
+import authRoutes from './routes/auth.routes.js';
+import noteRoutes from './routes/note.routes.js';
+import { errorHandler } from './middlewares/error.middleware.js';
+
+//routes
+app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/notes', noteRoutes);
+
+app.use(errorHandler);
+
+export default app;
