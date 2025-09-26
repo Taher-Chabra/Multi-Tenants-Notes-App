00e22d44405dbd1242445c2508dd7404fae01c9d
Taher Chabra <redsign12345@gmail.com>
2025-09-26 18:23:08 +0530

    refactor: update apiRequest function

--- frontend/src/utils/requestHandler.ts
+++ frontend/src/utils/requestHandler.ts
@@ -2,32 +2,32 @@ import { refreshAccessToken } from "@/services/auth.service";
 import { redirect } from "next/navigation";
 
 const apiRequest = async (url: string, options: RequestInit = {}) => {
-   const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
-   const response = await fetch(`${baseURL}${url}`, {
-      ...options,
-      credentials: 'include',
-   });
+  const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
+  let response = await fetch(`${baseURL}${url}`, {
+    ...options,
+    credentials: "include",
+  });
 
-   if (response.status === 401) { 
-      const refreshResponse = await refreshAccessToken();
-      if (refreshResponse.ok) {
-         const retryResponse = await fetch(`${baseURL}${url}`, {
-            ...options,
-            credentials: 'include',
-         });
-      } else {
-         redirect('/auth/login');
-      }
-   }
+  if (response.status === 401) {
+    const refreshResponse = await refreshAccessToken();
+    if (refreshResponse.ok) {
+      response = await fetch(`${baseURL}${url}`, {
+        ...options,
+        credentials: "include",
+      });
+    } else {
+      redirect("/auth/login");
+    }
+  }
 
-   const data = await response.json().catch(() => null);
-   
-   if (!response.ok) {
-      const message = data?.message || 'An error occurred';
-      throw new Error(message);
-   }
+  const data = await response.json().catch(() => null);
 
-   return data;
-}
+  if (!response.ok) {
+    const message = data?.message || "An error occurred";
+    throw new Error(message);
+  }
+
+  return data;
+};
 
 export { apiRequest };
